d57b33ab31586ca530b5287c3b65340e3d619e7c
stautuan <stautuan89@gmail.com>
2024-06-08 21:58:46 -0400

    Completed the core logic of the game

--- script.js
+++ script.js
@@ -2,10 +2,36 @@
 
 const input = document.querySelector('input');
 const form = document.querySelector('form');
+const output = document.querySelector('.output');
+
+// Generate a random secret code when the game is initialized
+const randomNum = Math.floor(Math.random() * 26) + 1;
+const secretCode = (randomNum + 9).toString(36).toUpperCase();
+
+console.log(secretCode);
+
+let attempts = 4;
 
 function getValue(e) {
   e.preventDefault();
-  console.log(input.value);
+
+  let guess = input.value.toUpperCase();
+
+  attempts--;
+
+  if (attempts === 0 && guess !== secretCode) {
+    output.innerHTML = `BOOOOOOOMMM... YOU BLEW IT! THE CORRECT CODE WAS "${secretCode}"`;
+    input.disabled = true;
+  } else if (guess > secretCode) {
+    output.innerHTML = 'EARLIER';
+  } else if (guess < secretCode) {
+    output.innerHTML = 'LATER';
+  } else {
+    output.innerHTML = 'TICK...FZZZZ...CLICK...YOU DID IT!';
+    input.disabled = true;
+  }
+
+  input.value = '';
 }
 
 form.addEventListener('submit', getValue);
